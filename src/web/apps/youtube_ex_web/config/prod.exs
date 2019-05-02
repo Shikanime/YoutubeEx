7b561c0a93e27076cd4076113a4c273b8b489e61
Shikanime Deva <deva.shikanime@protonmail.com>
2019-05-02 14:02:42 +0200

    Remove cache manifest

--- src/web/apps/youtube_ex_web/config/prod.exs
+++ src/web/apps/youtube_ex_web/config/prod.exs
@@ -1,7 +1,6 @@
 use Mix.Config
 
 config :youtube_ex_web, YoutubeExWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   root: ".",
   version: Application.spec(:phoenix_distillery, :vsn)
