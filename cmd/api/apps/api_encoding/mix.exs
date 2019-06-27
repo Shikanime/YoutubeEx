462c623df9614313535b82616906e284b7de13fc
Shikanime Deva <deva.shikanime@protonmail.com>
2019-06-28 01:10:11 +0200

    Fix path and remove useless comments

--- cmd/api/apps/api_encoding/mix.exs
+++ cmd/api/apps/api_encoding/mix.exs
@@ -15,7 +15,6 @@ defmodule Api.Encoding.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -23,14 +22,10 @@ defmodule Api.Encoding.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:poolboy, "~> 1.5"},
       {:erlport, "~> 0.10.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true}
     ]
   end
 end
--- cmd/api/apps/api_encoding/test/youtube_ex_encoding_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Api.EncodingTest do
-  use ExUnit.Case
-  doctest Api.Encoding
-
-  test "greets the world" do
-    assert Api.Encoding.hello() == :world
-  end
-end
